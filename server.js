aef025915477d720c2cf044490bf6864ec71b8c2
hp <aadeez.13@gmail.com>
2018-03-24 01:39:26 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express= require('express');
+const fs=require('fs');
+var app = express();
+
+const hbs=require('hbs');
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+app.use((request,response,next)=>{
+	var now = new Date().toString();
+	console.log(now +':'+request.method,request.url);    //other methods for information for middleware
+	var log=now + request.method+request.url + '\n';
+	fs.appendFile('server.log',log,(err)=>{
+		if(err){
+			console.log('Unable to append server.log file');
+		}
+	});
+	next();
+})
+
+// app.get('/',(req,res)=>{
+// 	res.render('maintainence.hbs');
+	 	
+// });
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',(request,response)=>{
+
+	response.render('home.hbs',{
+		Logo:'Welcome dear',
+		data:{
+		name:'Adnan',
+		likes:['Gymming',
+				'Cycling'
+				]}
+	});
+});
+
+hbs.registerHelper('currentTime',()=>{
+	return new Date().toString();
+});
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear();
+});
+hbs.registerHelper('ScreamIt',(text)=>{
+	return text.toUpperCase();
+});
+app.get('/about',(request,response)=>{
+	response.render('about.hbs',{
+		pagetitle:'About page'
+	});
+});
+
+app.get('/about/bad',(request,response)=>{
+	response.send('bad parameter send!');
+});
+
+app.listen(3000,()=>{
+	console.log('Server is up on port 3000!');
+});
